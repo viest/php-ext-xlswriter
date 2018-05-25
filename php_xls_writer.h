b9aa0901ea74ade98a4533e8ea84cef8106036b2
viest <dev@service.viest.me>
2018-05-25 17:06:44 +0800

    VERSION 1.0.1

--- php_xls_writer.h
+++ php_xls_writer.h
@@ -18,7 +18,7 @@
 extern zend_module_entry xlswriter_module_entry;
 #define phpext_xlswriter_ptr &xlswriter_module_entry
 
-#define PHP_XLSWRITER_VERSION "1.0.0"
+#define PHP_XLSWRITER_VERSION "1.0.1"
 
 #ifdef PHP_WIN32
 #	define PHP_VTIFUL_API __declspec(dllexport)

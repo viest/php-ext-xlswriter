4aa86eda0ea83f19b6f3088b6a032906bb503d0d
viest <dev@service.viest.me>
2019-09-04 23:55:58 +0800

    Fix(property): return val

--- include/excel.h
+++ include/excel.h
@@ -18,10 +18,11 @@
 #define V_XLS_COF    "config"
 #define V_XLS_PAT    "path"
 
-#define GET_CONFIG_PATH(dir_path_res, class_name, object)                                             \
-    do {                                                                                              \
-        zval *_config  = zend_read_property(class_name, object, ZEND_STRL(V_XLS_COF), 0, NULL);     \
-        (dir_path_res) = zend_hash_str_find(Z_ARRVAL_P(_config), ZEND_STRL(V_XLS_PAT));             \
+#define GET_CONFIG_PATH(dir_path_res, class_name, object)                                          \
+    do {                                                                                           \
+        zval rv;                                                                                   \
+        zval *_config  = zend_read_property(class_name, object, ZEND_STRL(V_XLS_COF), 0, &rv);     \
+        (dir_path_res) = zend_hash_str_find(Z_ARRVAL_P(_config), ZEND_STRL(V_XLS_PAT));            \
     } while(0)
 
 extern zend_class_entry *vtiful_xls_ce;
